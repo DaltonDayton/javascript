f33cf664e88d576b6b9df83b5d31907432f40775
Dalton Dayton <daltondayton1@gmail.com>
2019-07-03 14:32:24 -0600

    Update Todo List

--- todolist/list.js
+++ todolist/list.js
@@ -5,12 +5,33 @@ window.setTimeout(function() {
 
     while (input !== "quit") {
         if (input === "list") {
-            console.log(todos);
+            listTodos();
         } else if (input === "new") {
-            let newTodo = prompt("Enter new todo");
-            todos.push(newTodo);
+            addTodo();
+        } else if (input === "delete") {
+            deleteTodo();
         }
         input = prompt("What would you like to do?");
     }
     console.log("You quit the app.");
+
+    function listTodos() {
+        console.log("**********");
+        todos.forEach(function(todo, i) {
+            console.log(i + ": " + todo);
+        });
+        console.log("**********");
+    }
+
+    function addTodo() {
+        let newTodo = prompt("Enter new todo");
+        todos.push(newTodo);
+        console.log("Added Todo");
+    }
+
+    function deleteTodo() {
+        let index = prompt("Enter index of todo");
+        todos.splice(index, 1);
+        console.log("Deleted Todo");
+    }
 }, 500);

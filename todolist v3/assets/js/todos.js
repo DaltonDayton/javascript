72b33884335560f06dc9bae8d1e7fdfea5e2d462
Dalton Dayton <daltondayton1@gmail.com>
2019-07-05 13:49:39 -0600

    add structure and functionality of todolist v3

---
+++ todolist v3/assets/js/todos.js
@@ -0,0 +1,26 @@
+// Check off specific todos by clicking
+$("ul").on("click", "li", function() {
+    $(this).toggleClass("completed");
+});
+
+// Click on X to delete Todo
+$("ul").on("click", "span", function(event) {
+    $(this)
+        .parent()
+        .fadeOut(500, function() {
+            $(this).remove();
+        });
+    event.stopPropagation();
+});
+
+// Event listner for text box
+$("input[type='text']").keypress(function(event) {
+    if (event.which === 13) {
+        // Grab todo text from input
+        var todoText = $(this).val();
+        // Clear input text
+        $(this).val("");
+        // Create a new lia nd add to ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
+});
